256311277cec0881fbf1aee8f1dbe418171ad92c
yutaplug <213663917+yutaplug@users.noreply.github.com>
2026-09-04 23:17:39 +0200

    Update build.gradle.kts

--- plugins/HideCallButtons/build.gradle.kts
+++ plugins/HideCallButtons/build.gradle.kts
@@ -1,2 +1,10 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Fork of HideCallButtons"
+aliucord {
+    changelog.set(
+        """
+        # 1.0.1
+        * Add option to hide camera button in vc
+        """.trimIndent(),
+    )
+}
